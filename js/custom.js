9f766376ee5555f96a35ab2e3e30eb97829719d5
YoonJY <133613780+yonnjy@users.noreply.github.com>
2023-07-25 15:56:47 +0900

    230725
    230725

---
+++ js/custom.js
@@ -0,0 +1,35 @@
+$(function () {
+
+    $('.Main__content').fullpage({
+        anchors: ['main', 'sub01', 'sub02'],
+        navigation: false,
+        css3: false,
+        //반응형에서 fullpage 안하기.
+        responsiveWidth: 700,
+        //넘치는 부분 스크롤 하기.
+        scrollOverflow: true,
+        //아랜 속도 부분... https://jqueryui.com/easing/ 참고.
+        easing: 'easeOutBounce',
+        //easingCss3: 'cubic-bezier(0.785, 0.135, 0.15, 0.86)',
+        afterRender: function () {
+            $('.Main__content .section').eq(0).addClass('on');
+        },
+        afterLoad: function (lnk, idx) {
+            console.log(lnk, idx);
+            // $('.gnb li').eq(idx - 1).addClass('on').siblings().removeClass('on');
+            $('.Main__content .section').eq(idx - 1).addClass('on').siblings().removeClass('on');
+        },
+        onLeave: function (idx, nidx, dir) {
+            $('.gnb li').eq(nidx - 1).addClass('on').siblings().removeClass('on');
+            console.log(idx, nidx, dir);
+
+            if (dir == 'up') {
+                $('.header').addClass('on')
+            } else {
+                $('.header').removeClass('on')
+            }
+
+        }
+    });
+
+})
